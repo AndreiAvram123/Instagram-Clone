af1e28ec346adb93b6e8a4be4b446aedc7c9652a
Andrei Avram <avramandreitiberiu@gmail.com>
2021-03-16 13:04:08 +0000

    Initial Commit

---
+++ Instagram-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Andrei Avram on 16/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
